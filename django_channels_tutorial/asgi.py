25a55d7028382b938835aefefb02ee4f293fd3a6
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2023-01-02 21:04:18 +0000

    Fix bug

--- django_channels_tutorial/asgi.py
+++ django_channels_tutorial/asgi.py
@@ -16,7 +16,6 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels_tutorial.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
